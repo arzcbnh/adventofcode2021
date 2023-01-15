56863568dd03d7855e885dd7ba9703d2ffb7969b
Matheus Henrique <matthpmoreira@gmail.com>
2023-01-15 14:17:21 -0300

    Finish day 3
    The structure of the project started confusing me, so I ended up borking
    the Makefile. That's worrying, because the project is really small.
    I'll be looking into other build systems to fix that next time. I added
    a vector and a string library, and to make things simpler, I'm trying to
    understand submodules. I'm not sure if I'm succeeding. I'm investing into
    better commit messages.

--- src/03.c
+++ src/03.c
@@ -1,87 +1,88 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#define INPUT_AMOUNT 1000
+#include "sds.h"
+#include "vec.h"
 
 typedef enum { CO2, O2 } Gas;
 
 static int
-get_common_bit(char *bins[], int count, int position, Gas detecting)
+find_common_bit(char **binaries_vec, int index, Gas detecting)
 {
 	int frequency = 0;
 
-	for (int i = 0; i < count; i++)
-		frequency += bins[i][position] - '0' ? 1 : -1;
+	for (int i = 0; i < vector_size(binaries_vec); i++)
+		frequency += binaries_vec[i][index] - '0' ? 1 : -1;
 
 	return detecting == CO2 && frequency < 0 || detecting == O2 && frequency >= 0;
 }
 
 static int
-find_rate(char *bins[], Gas detecting)
+find_rate(char **binaries_vec, Gas detecting)
 {
-	char *copy[INPUT_AMOUNT] = { 0 };
-	for (int i = 0; i < INPUT_AMOUNT; i++)
-		copy[i] = bins[i];
+	char **copy_vec = vector_create();
+	
+	for (int i = 0; i < vector_size(binaries_vec); i++)
+		vector_add(&copy_vec, binaries_vec[i]);
 
-	int count = INPUT_AMOUNT;
-	for (int position = 0; count > 1; position++) {
-		char common = get_common_bit(copy, count, position, detecting) + '0';
-		int remain = 0;
+	for (int index = 0; vector_size(copy_vec) > 1; index++) {
+		char common = find_common_bit(copy_vec, index, detecting) + '0';
+		int remaining = 0;
 
-		for (int i = 0; i < count; i++)
-			if (copy[i][position] == common)
-				copy[remain++] = copy[i];
-
-		count = remain;
+		for (int i = 0; i < vector_size(copy_vec); i++)
+			if (copy_vec[i][index] != common)
+				vector_remove(&copy_vec, i--);
 	}
 
-	int length = strlen(*bins);
 	int n = 0;
-	for (int i = 0; i < length; i++)
-		n = copy[0][i] - '0' ? ~(~n << 1) : n << 1;
 
+	for (int i = 0; i < sdslen(*binaries_vec); i++)
+		n = (*copy_vec)[i] - '0' ? ~(~n << 1) : n << 1;
+
+	vector_free(copy_vec);
 	return n;
 }
 
 static int
-find_gamma(char *bins[])
+find_gamma(char **binaries_vec)
 {
-	int length = strlen(*bins);
 	int n = 0;
 
-	for (int i = 0; i < length; i++)
-		n = get_common_bit(bins, INPUT_AMOUNT, i, O2) ? ~(~n << 1) : n << 1;
+	for (int i = 0; i < sdslen(*binaries_vec); i++)
+		n = find_common_bit(binaries_vec, i, O2) ? ~(~n << 1) : n << 1;
 
 	return n;
 }
 
 static void
-input_data(char *bins[])
+input_data(char ***binaries_vec)
 {
-	char bin[16];
+	char *bin = NULL;
+	size_t len = 0;
 
-	for (int i = 0; i < INPUT_AMOUNT; i++) {
-		fgets(bin, 16, stdin);
-		bin[strcspn(bin, "\n")] = 0;
-		bins[i] = strdup(bin);
+	while (getline(&bin, &len, stdin) > 0) {
+		sds bin_sds = sdsnew(bin);
+		sdstrim(bin_sds, "\n");
+		vector_add(binaries_vec, bin_sds);
 	}
+
+	free(bin);
 }
 
 void
 day03(void)
 {
-	char *bins[INPUT_AMOUNT] = { 0 };
-	input_data(bins);
+	char **binaries_vec = vector_create();
+	input_data(&binaries_vec);
 
-	int gamma = find_gamma(bins);
-	int epsilon = ~(~0U << strlen(*bins)) ^ gamma;
-	int o2_rate = find_rate(bins, O2);
-	int co2_rate = find_rate(bins, CO2);
+	int gamma = find_gamma(binaries_vec);
+	int epsilon = ~(~0U << sdslen(*binaries_vec)) ^ gamma;
+	int o2_rate = find_rate(binaries_vec, O2);
+	int co2_rate = find_rate(binaries_vec, CO2);
 
 	printf("Part 1: %u\n", gamma * epsilon);
 	printf("Part 2: %u\n", o2_rate * co2_rate);
-	
-	for (int i = 0; i < INPUT_AMOUNT; i++)
-		free(bins[i]);
+
+	for (int i = 0; i < vector_size(binaries_vec); i++)
+		sdsfree(binaries_vec[i]);
+	vector_free(binaries_vec);
 }
