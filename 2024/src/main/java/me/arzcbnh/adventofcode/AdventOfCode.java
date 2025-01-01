cde3c534a2af540e05d07bcfcdf448d16a12510b
Matheus Henrique <matthpmoreira@gmail.com>
2025-01-01 01:36:28 -0300

    refactor(2024): update support for `day` package

--- 2024/src/main/java/me/arzcbnh/adventofcode/AdventOfCode.java
+++ 2024/src/main/java/me/arzcbnh/adventofcode/AdventOfCode.java
@@ -1,5 +1,6 @@
 package me.arzcbnh.adventofcode;
 
+import me.arzcbnh.adventofcode.day.SolverFactory;
 import me.arzcbnh.adventofcode.days.DayFactory;
 import me.arzcbnh.adventofcode.input.InputProvider;
 import me.arzcbnh.adventofcode.input.InputServiceFactory;
@@ -12,11 +13,22 @@ public class AdventOfCode {
         boolean useExample = args.length > 1;
 
         String input = useExample ? inputProvider.getExample() : inputProvider.getInput(day);
-        var dayInstance = DayFactory.createDay(day);
-        var answer = dayInstance.solve(input);
+        String part1, part2;
+
+        try {
+            var dayInstance = DayFactory.createDay(day);
+            var answer = dayInstance.solve(input);
+            part1 = answer.part1();
+            part2 = answer.part2();
+        } catch (Exception e) {
+            var solverInstance = SolverFactory.createDay(day);
+            var answer = solverInstance.solve(input);
+            part1 = answer.part1();
+            part2 = answer.part2();
+        }
 
         System.out.println("--- Advent of Code 2024, day " + day + " ---");
-        System.out.println("Part 1: " + answer.part1());
-        System.out.println("Part 2: " + answer.part2());
+        System.out.println("Part 1: " + part1);
+        System.out.println("Part 2: " + part2);
     }
 }
