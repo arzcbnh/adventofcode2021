6aac9854a8abcbfa877d272a5c2f8926b74fb6cb
Matheus Henrique <matthpmoreira@gmail.com>
2025-01-03 21:37:39 -0300

    feat(2024/7): complete

---
+++ 2024/src/main/java/me/arzcbnh/adventofcode/day/day7/Operation.java
@@ -0,0 +1,9 @@
+// package me.arzcbnh.adventofcode.day.day7;
+//
+// final class Operation {
+//    private final Integer number;
+//    private final Operation operation;
+//    private final Operator operator;
+//
+//
+// }
---
+++ 2024/src/main/java/me/arzcbnh/adventofcode/day/day7/Operator.java
@@ -0,0 +1,7 @@
+package me.arzcbnh.adventofcode.day.day7;
+
+enum Operator {
+    Addition,
+    Multiplication,
+    Concatenation
+}
