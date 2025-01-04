b73c85c93c906c9e5e6201121e2dacff3aa26513
Matheus Henrique <matthpmoreira@gmail.com>
2025-01-04 12:20:27 -0300

    refactor(2024/7): clean up

--- 2024/src/main/java/me/arzcbnh/adventofcode/day/day7/Operation.java
+++
@@ -1,9 +0,0 @@
-// package me.arzcbnh.adventofcode.day.day7;
-//
-// final class Operation {
-//    private final Integer number;
-//    private final Operation operation;
-//    private final Operator operator;
-//
-//
-// }
